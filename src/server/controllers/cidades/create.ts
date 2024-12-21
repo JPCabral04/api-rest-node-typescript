6b9fdaa8242b63a68ddcb247e34d59d32aeb3d2b
JPCabral04 <jpqcabral04@gmail.com>
2024-12-21 19:05:19 -0300

    feat: remove estado and filter validation from city creation schema

--- src/server/controllers/cidades/create.ts
+++ src/server/controllers/cidades/create.ts
@@ -7,11 +7,6 @@ import { validation } from "../../shared/middleware";
 // Interface para definir o formato da cidade
 interface ICidade {
     nome: string;
-    estado: string;
-}
-
-interface IFilter {
-    filter : string;
 }
 
 // Esquema de validação usando yup
@@ -19,10 +14,7 @@ interface IFilter {
 export const createValidation = validation((getSchema) =>({
     body : getSchema<ICidade>(yup.object().shape({
         nome: yup.string().required().min(3),
-        estado: yup.string().required().min(3),
-    })),
-    query : getSchema<IFilter>(yup.object().shape({
-        filter: yup.string().required().min(3),
+        
     })),
 }));
 
@@ -31,7 +23,7 @@ export const create = async (req: Request, res: Response) => {
 
     console.log(req.body);
 
-    res.status(StatusCodes.CREATED).send("Create!")
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).send("Não Implementado!");
 
 };
 
