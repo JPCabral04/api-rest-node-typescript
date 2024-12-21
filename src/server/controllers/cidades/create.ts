4ac3139978ece915c95d78854162f689a41689e7
JPCabral04 <jpqcabral04@gmail.com>
2024-12-21 12:48:26 -0300

    feat: add yup validation for city creation endpoint

--- src/server/controllers/cidades/create.ts
+++ src/server/controllers/cidades/create.ts
@@ -1,14 +1,41 @@
 import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import * as yup from 'yup';
 
-
+// Interface para definir o formato da cidade
 interface ICidade {
     nome: string;
 }
 
-export const create = (req: Request<{},{},ICidade>, res: Response) => {
+// Esquema de validação usando yup
+const bodyValidation: yup.Schema<ICidade> = yup.object().shape({
+    nome: yup.string().required().min(3),
+});
 
-    console.log(req.body);
-    
+// Função de criação
+export const create = async (req: Request, res: Response) => {
+    try {
+        
+        const validateData = await bodyValidation.validate(req.body, { abortEarly: false });
 
-    res.send('Create!');
-}
+        console.log(validateData);
+
+        res.status(StatusCodes.CREATED).json({
+            message: 'Cidade criada com sucesso!',
+            data: validateData,
+        });
+
+        return; 
+    } catch (error) {
+        const yupError = error as yup.ValidationError;
+
+        res.status(StatusCodes.BAD_REQUEST).json({
+            errors: yupError.inner.map(err => ({
+                path: err.path,
+                message: err.message,
+            })),
+        });
+
+        return; 
+    }
+};
